7daffa93a543f16445915885b503f9664b2d66ac
Mohammad Hossein Mangelian <h.m.mnglean@gmail.com>
2023-04-08 21:43:53 +0430

    basic start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <div className="container">
+      <h1 className='text-red-500 text-3xl'>Hello world!</h1>
+    </div>
+  )
+}
+
+export default App

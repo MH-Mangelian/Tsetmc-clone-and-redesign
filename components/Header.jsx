1c9a55f63b2f5ee169d5516e3ef361407189d25c
Mohammad Hossein Mangelian <h.m.mnglean@gmail.com>
2023-04-15 20:28:35 +0430

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,8 +4,8 @@ import { useEffect, useState } from "react";
 import { FcHeatMap } from "react-icons/fc";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { FaClipboardList } from "react-icons/fa";
-import { MdFiberNew,MdHome,MdClose } from "react-icons/md";
-import { GiOpenBook,GiEarthAfricaEurope } from "react-icons/gi";
+import { MdFiberNew, MdHome, MdClose } from "react-icons/md";
+import { GiOpenBook, GiEarthAfricaEurope } from "react-icons/gi";
 import {
   BsFillQuestionCircleFill,
   BsFillChatRightTextFill,
@@ -17,8 +17,8 @@ import {
 
 const Header = () => {
   // ------------------ HamburgerMenu -----------------------------
-  const [toggle,setToggle] = useState(false);
-  
+  const [toggle, setToggle] = useState(false);
+
   // ----------switch Mode : dark/light --------------------------
   const [darkMode, setDarkMode] = useState(true);
 
@@ -79,24 +79,119 @@ const Header = () => {
             <FaClipboardList className="w-6 h-7  text-black/90 dark:text-white jumper-hover" />
           </div>
           <div className="sm:hidden flex items-center">
-            <div>
-                {
-                    toggle ? 
-                    <MdClose className="w-7 h-7 mx-2 text-black/90 dark:text-white jumper-hover hover:text-blue-700"
-                    onClick={() => setToggle(!toggle)}/>
-                    :
-                    <RxHamburgerMenu className="w-7 h-7 mx-2 text-black/90 dark:text-white jumper-hover hover:text-blue-700"
-                    onClick={() => setToggle(!toggle)}/>
-                }
+            <div className="">
+              {toggle ? (
+                <MdClose
+                  className="w-7 h-7 mx-2 text-black/90 dark:text-white jumper-hover hover:text-blue-700"
+                  onClick={() => setToggle(!toggle)}
+                />
+              ) : (
+                <RxHamburgerMenu
+                  className="w-7 h-7 mx-2 text-black/90 dark:text-white jumper-hover hover:text-blue-700"
+                  onClick={() => setToggle(!toggle)}
+                />
+              )}
             </div>
 
-            <div className={`${toggle ? "flex" : "hidden"} p-4  bg-gradient-to-l from-white/25 to-white/80 dark:bg-gradient-to-l dark:from-slate-800 dark:to-slate-600 `}>
-                <ul>
-                    <li>hiiii</li>
-                </ul>
+            <div
+              className={`${
+                toggle ? "flex" : "hidden"
+              } p-4 top-20 right-0 mx-4 rounded-lg shadow-md bg-gradient-to-l from-white/25 to-white/80 dark:bg-gradient-to-l dark:from-slate-800 dark:to-slate-700/60 `}
+            >
+              <ul className="">
+                <li className="flex">
+                  <a
+                    href=""
+                    className="flex text-lg font-medium my-1 space-x-1.5 bg-slate-600 py-1.5 px-2 rounded-md"
+                  >
+                    <p>اخبار</p>
+                    <MdFiberNew className="w-8 h-8 my-0.5 text-black/90 dark:text-white jumper-hover" />
+                  </a>
+                </li>
+                <li className="flex my-4">
+                  <a
+                    href=""
+                    className="flex text-lg font-medium my-1 space-x-1.5 bg-slate-600 py-1.5 px-2 rounded-md"
+                  >
+                    <p>راهنما</p>
+                    <BsFillQuestionCircleFill className="w-6 h-7 my-0.5 text-black/90 dark:text-white jumper-hover" />
+                  </a>
+                </li>
+                <li className="flex my-4">
+                  <a
+                    href=""
+                    className="flex text-lg font-medium my-1 space-x-1.5 bg-slate-600 py-1.5 px-2 rounded-md"
+                  >
+                    <p>ارتباط با ما</p>
+                    <BsFillChatRightTextFill className="w-6 h-7 my-0.5 text-black/90 dark:text-white jumper-hover" />
+                  </a>
+                </li>
+                <li className="flex my-4">
+                  <a
+                    href=""
+                    className="flex text-lg font-medium my-1 space-x-1.5 bg-slate-600 py-1.5 px-2 rounded-md"
+                  >
+                    <p>نقشه بازار</p>
+                    <FcHeatMap className="w-7 h-7 my-0.5 text-black/90 dark:text-white jumper-hover" />
+                  </a>
+                </li>
+                <li className="flex my-4">
+                  <a
+                    href=""
+                    className="flex text-lg font-medium my-1 space-x-1.5 bg-slate-600 py-1.5 px-2 rounded-md"
+                  >
+                    <p>آموزش</p>
+                    <GiOpenBook className="w-7 h-7  text-black/90 dark:text-white jumper-hover" />
+                  </a>
+                </li>
+                <li className="flex my-4">
+                  <a
+                    href=""
+                    className="flex text-lg font-medium my-1 space-x-1.5 bg-slate-600 py-1.5 px-2 rounded-md"
+                  >
+                    <p>جستجوی نماد</p>
+                    <BsSearch className="w-6 h-7  text-black/90 dark:text-white jumper-hover" />
+                  </a>
+                </li>
+                <li className="flex my-4">
+                  <a
+                    href=""
+                    className="flex text-lg font-medium my-1 space-x-1.5 bg-slate-600 py-1.5 px-2 rounded-md"
+                  >
+                    <p>دیده بان بازار</p>
+                    <BsTable className="w-6 h-7  text-black/90 dark:text-white jumper-hover" />
+                  </a>
+                </li>
+                <li className="flex my-4">
+                  <a
+                    href=""
+                    className="flex text-lg font-medium my-1 space-x-1.5 bg-slate-600 py-1.5 px-2 rounded-md"
+                  >
+                    <p>خانه</p>
+                    <MdHome className="w-7 h-7  text-black/90 dark:text-white jumper-hover" />
+                  </a>
+                </li>
+                <li className="flex my-4">
+                  <a
+                    href=""
+                    className="flex text-lg font-medium my-1 space-x-1.5 bg-slate-600 py-1.5 px-2 rounded-md"
+                  >
+                    <p>Languages</p>
+                    <GiEarthAfricaEurope className="w-6 h-7  text-black/90 dark:text-white jumper-hover" />
+                  </a>
+                </li>
+                <li className="flex my-4">
+                  <a
+                    href=""
+                    className="flex text-lg font-medium my-1 space-x-1.5 bg-slate-600 py-1.5 px-2 rounded-md"
+                  >
+                    <p>سایر خدمات</p>
+                    <FaClipboardList className="w-6 h-7  text-black/90 dark:text-white jumper-hover" />
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
-
         </div>
       </div>
     </header>
